83f3a100bad9a0339db6bebeab2eb7ed81569ac2
Emilio Conde <econde@npitx.org>
2022-09-17 15:25:38 -0500

    texas flag done

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,8 +3,9 @@ import "./style.css"
 const Title = () => {
   return (
     <div className='title'>
-        <h1 id='titleh1'>Ten Musical Geniuses from the State of Texas</h1>
-        <img className='titleImg1'/>
+      <img className='titleImg1'/>
+      <h1 id='titleh1'>Ten Musical Geniuses from the State of Texas</h1>
+        <div className='redTexasFlagElement'/>
     </div>
   )
 }
